fe40bd4b5097c56514b3a5e0a00b7338a6c49ab5
Angel Young <angel.m.young24@gmail.com>
2018-02-21 12:39:32 -0500

    changes from line 15 to 38. commented out so I can focus on the array

--- index.js
+++ index.js
@@ -12,30 +12,6 @@ for (var i = 0; i < currentCharacter.length; i++) {
     charactersArray[i] = "_";
 }
 
-var lettersLeft = words.length;
-
-
-var words = document.getElementById('box1').innerHTML
-
-
-// alphabet
-var qwerty = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-for(let i=0;i<qwerty.length;i++){
-	var div = document.createElement('div');
-	var letters = document.createTextNode(qwerty[i])
-	div.appendChild(letters);
-	div.style.width='40px';
-	div.style.height='40px';
-	div.style.border='2px solid';
-	div.style.margin='10px';
-	div.style.display='inline-block';
-	div.classList.add('box');
-	document.body.appendChild(div);
-	var boxes = document.getElementsByClassName('box');
-	boxes[i].addEventListener('click', function(){
-				showLetter(i)
-	})
-}
 
 function showLetter(currentItem){
 	console.log(qwerty[currentItem])
