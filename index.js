7c9000a4731c6654bf2747ba352a7d8e4724a5a1
Angel Young <angel.m.young24@gmail.com>
2018-03-14 02:03:05 -0400

    changes to index.js file

--- index.js
+++ index.js
@@ -1,128 +1,178 @@
-var degrassi = [{
+var degrassi = [
+    {
         name: "Maya",
         hint: "Their sister visits from college due to a tragedy"
-    },
-    {
+    }, {
         name: "Zig",
         hint: "Only character to be in a polyamourus relationship"
-    },
-    {
+    }, {
         name: "Lola",
         hint: "This character had an abortion"
-    },
-    {
+    }, {
         name: "Frankie",
         hint: "This character has a twin"
-    },
-    {
+    }, {
         name: "Yael",
         hint: "This character is generqueer"
-    },
-    {
+    }, {
         name: "Tiny",
         hint: "This character was in a gang with his brother"
-    },
-    {
+    }, {
         name: "Zoe",
-        hint: "This character was on an actor on West Drive",
-    },
-    {
+        hint: "This character was on an actor on West Drive"
+    }, {
         name: "Tristan",
         hint: "This character was in a coma after a bus crash"
-    },
-    {
+    }, {
         name: "Grace",
         hint: "This character has cystic fibrosis"
-    },
-    {
+    }, {
         name: "Esme",
         hint: "This character pushed their significant other down a hill"
-    },
-    {
+    }, {
         name: "Winston",
         hint: "This characters nickname is Chewy"
-    },
-    {
+    }, {
         name: "Hunter",
         hint: "This character caused a school lockdown"
-    },
-    {
+    }, {
         name: "Vijay",
         hint: 'First line: "Oh My God! I love high school."'
-    },
-    {
+    }, {
         name: "Rasha",
         hint: "This character was prom queen with their significant other"
-    },
-    {
+    }, {
         name: "Goldi",
         hint: "This character is president of the feminist club"
-    },
-    {
+    }, {
         name: "Jonah",
         hint: "This character has a daughter"
-    },
-    {
+    }, {
         name: "Baaz",
         hint: "This character has an older sister who graduated in 2016"
-    },
-    {
+    }, {
         name: "Miles",
         hint: "This characters father is abusive"
-    },
-    {
+    }, {
         name: "Saad",
-        hint: "This characters nickname is Creepy Camera Guy",
-    },
-    {
+        hint: "This characters nickname is Creepy Camera Guy"
+    }, {
         name: "Shay",
         hint: "This character was in a love triangle with their best friend"
     }
 ];
 
-
-
 // select a random character
-var random = degrassi[Math.floor(Math.random() * degrassi.length)];
+var random = Math.floor(Math.random() * degrassi.length);
+// console.log(degrassi[random])
 
-degrassi[random].name //gives you the name
-degrassi[random].hint //gives you its corresponding hint
-
+var currentWord = degrassi[random].name //name
+var currentHint = degrassi[random].hint //corresponding hint
+var showAnswer = document.getElementById("answer");
+var showHint = document.getElementById("hint");
+var showLives = document.getElementById("lives");
+var checkWord = currentWord.split("");
 
 
 // the array that loops through characters
 var answer = [];
-for (var i = 0; i < degrassi.length; i++) {
+for (var i = 0; i < currentWord.length; i++) {
     answer[i] = "_";
+    // console.log(answer)
+}
+showAnswer.innerText = answer.join(" ");
+showLives.innerHTML = "You have " + lives + " lives left";
+
+// array to loop through letters
+function check(qwerty) {
+    for (i = 0; i < currentWord.length; i++) {
+        // console.log(qwerty); console.log(currentWord);
+
+        if (qwerty === currentWord[i].toLowerCase()) {
+            answer[i] = qwerty;
+            degrassi.innerHTML = answer.join(" ");
+            targetHint();
+
+        }
+
+    }
+}
+// targets hint after correct first word
+function targetHint() {
+
+    showAnswer.innerText = answer.join(" ");
+    showHint.innerText = currentHint;
+
 }
 
-var lettersLeft = degrassi.length;
+lives = 6;
+
+function checkWin() {
+
+    if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+    }
+
+    if (checkWord(lives.value) == false) {
+        lives--;
+        showLives.innerHTML = "You Win!";
+    }
+}
 
 
-var degrassi = document.getElementById("degrassi");
 
+window.onload = check(qwerty)
 
+var qwerty = [
+    'q',
+    'w',
+    'e',
+    'r',
+    't',
+    'y',
+    'u',
+    'i',
+    'o',
+    'p',
+    'a',
+    's',
+    'd',
+    'f',
+    'g',
+    'h',
+    'j',
+    'k',
+    'l',
+    'z',
+    'x',
+    'c',
+    'v',
+    'b',
+    'n',
+    'm'
+];
 
+for (let i = 0; i < qwerty.length; i++) {
+    var div = document.createElement('div');
+    var letters = document.createTextNode(qwerty[i])
+    div.appendChild(letters);
+    div.style.width = '30px';
+    div.style.height = '30px';
+    div.style.border = '2px solid';
+    div.style.margin = '10px';
+    div.style.display = 'inline-block';
+    div.style.textAlign = 'center';
+    div
+        .classList
+        .add('box');
+    document
+        .body
+        .appendChild(div);
+    var boxes = document.getElementsByClassName('box');
 
-// // alphabet
-// var qwerty = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-// for(let i=0;i<qwerty.length;i++){
-// 	var div = document.createElement('div');
-// 	var letters = document.createTextNode(qwerty[i])
-// 	div.appendChild(letters);
-// 	div.style.width='40px';
-// 	div.style.height='40px';
-// 	div.style.border='2px solid';
-// 	div.style.margin='10px';
-// 	div.style.display='inline-block';
-// 	div.classList.add('box');
-// 	document.body.appendChild(div);
-// 	var boxes = document.getElementsByClassName('box');
-// 	boxes[i].addEventListener('click', function(){
-// 				showLetter(i)
-// 	})
-// }
-
-// function showLetter(currentItem){
-// 	console.log(qwerty[currentItem])
-// }
+    boxes[i].addEventListener("click", function (e) {
+        // check(the value of that boxes letter)
+        check(this.innerHTML);
+        boxes[i].style.backgroundColor = 'grey';
+    });
+}
